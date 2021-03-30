b5d4e87396572cb14842d71f9cf6f5e45c3a9401
Ludovici <ludovici.work@gmail.com>
2021-03-30 21:42:24 +0200

    created website hero

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,71 @@
+import React from 'react'
 import Head from 'next/head'
+import { logo } from '../utils/svg'
 
 export default function Home() {
   return (
-    <div>
+    <React.Fragment>
+
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
       </Head>
 
       <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <header>
+          <nav>
 
-        <p>
-          Get started by editing{' '}
-          <code>pages/index.js</code>
-        </p>
+            <div className="menu-desktop">
+              <div className="logo">{logo}</div>
+              <div className="links">
+                <a href="#">Mobile  <i className="fas fa-chevron-down"></i></a>
+                <a href="#">Company <i className="fas fa-chevron-down"></i></a>
+                <a href="#">Connect <i className="fas fa-chevron-down"></i></a>
+              </div>
+            </div>
+            <div className="right">
+              <a href="#">Login</a>
+              <a href="#" className="button">Sign Up</a>
+            </div>
+          </nav>
+          <div className="hero">
+            <h1>A mobile publishing platform</h1>
+            <p>Grow your audience and build your online brand</p>
+            <div className="ctagroup">
+              <a href="#" className="button">Start for free</a>
+              <a href="#" className="button outline">Learn more</a>
+            </div>
+          </div>
+        </header>
 
-        <div>
-          <a href="https://nextjs.org/docs">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <section className="features">
+          <h2>Designed for the future</h2>
 
-          <a href="https://nextjs.org/learn">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-           
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <div className="container">
+            <div className="pars">
+            <div className="paragraph 1">
+              <h3>Introducing an extensible editor</h3>
+              <p>
+                Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.
+              </p>
+            </div>
+            <div className="paragraph 2">
+              <h3>Robust content management</h3>
+              <p>
+                Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
+              </p>
+            </div>
+            </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-           
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+
+          <div className="img"></div>
+          </div>
+
+
+
+        </section>
+
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" />
-        </a>
-      </footer>
-    </div>
+    </React.Fragment>
   )
 }
